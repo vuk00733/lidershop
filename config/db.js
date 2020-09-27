a09dcaf5d8f6d49c54c921487f83efc6a69bd3ce
Вук Ивановић <iv180367d@student.etf.bg.ac.rs>
2020-09-27 21:35:21 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://vuk12345:vuk12345@myinfo.3qmcd.mongodb.net/<dbname>?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
